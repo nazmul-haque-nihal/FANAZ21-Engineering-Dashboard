157ba34f1b9567831cabd738909c1bf51166b3f5
master <master@master.error>
2025-09-06 11:53:01 -0400

    Remove all files except README.md and run.py

--- app/modeling/__init__.py
+++
@@ -1,7 +0,0 @@
-# app/modeling/__init__.py
-from .signal_modeling import SignalModeler
-from .circuit_modeling import CircuitModeler
-from .system_modeling import SystemModeler
-from .visualizations import create_modeling_plots
-
-__all__ = ['SignalModeler', 'CircuitModeler', 'SystemModeler', 'create_modeling_plots']
--- app/simulation/__init__.py
+++
@@ -1,7 +0,0 @@
-# app/simulation/__init__.py
-from .signal_simulation import SignalSimulator
-from .circuit_simulation import CircuitSimulator
-from .control_simulation import ControlSimulator
-from .visualizations import create_simulation_plots
-
-__all__ = ['SignalSimulator', 'CircuitSimulator', 'ControlSimulator', 'create_simulation_plots']
